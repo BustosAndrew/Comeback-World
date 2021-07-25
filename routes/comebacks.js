67fcaf18c21df0437d42acf32c6fe13122a9f4fd
BustosMan <bustosman@echo-studios.co.uk>
2021-07-24 20:25:54 -0700

    getting comebacks from the server side

--- routes/comebacks.js
+++ routes/comebacks.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 router.get('/comeback', async (req, res) => {
     const token = process.env.API_KEY;
-    res.send(token);
+    const url = 'https://generatorfun.com/consumeapi.php?api=';
+    axios
+        .get(url + token)
+        .then((response) => {
+            res.send(response.data);
+        })
+        .catch((error) => {
+            res.send(error);
+        });
 });
 
 module.exports = router;
