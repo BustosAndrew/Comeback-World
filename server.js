b29a1cd48e2df0d7beb086bc3d7d6881c22283fb
BustosMan <bustosman@echo-studios.co.uk>
2021-07-24 22:43:39 -0700

    set origin header

--- server.js
+++ server.js
@@ -10,8 +10,12 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const corsOptions = {
+    origin: 'https://comebackworld.netlify.app/',
+};
+
 // express middlewares
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
