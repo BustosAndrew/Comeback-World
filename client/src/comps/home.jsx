0f65492737fb2d101d1b26cbc38f32027a18e94a
BustosMan <bustosman@echo-studios.co.uk>
2021-08-15 21:52:15 -0700

    initial ui/ux update

--- client/src/comps/home.jsx
+++ client/src/comps/home.jsx
@@ -5,33 +5,36 @@ import { Link } from 'react-router-dom';
 export const Home = () => {
     return (
         <div className="home">
-            <section style={{ textAlign: 'center', paddingTop: '20px' }}>
-                <h1 style={{ color: 'whitesmoke' }}>
-                    Welcome to Comeback World
-                </h1>
-                <h4 style={{ color: 'whitesmoke' }}>
-                    The reddit of comebacks ;).
-                </h4>
-                <Link to="/signup" id="signup">
-                    <p>Sign Up</p>
-                </Link>
-            </section>
             <aside>
-                <div id="ex1">Generator Ex</div>
-                <div id="ex2">Forums Example</div>
+                <div id="card">
+                    <div id="intro">
+                        <h1 style={{ color: 'var(--dark-font)' }}>
+                            Welcome to Comeback World
+                        </h1>
+                        <h4 style={{ color: 'var(--dark-font)' }}>
+                            The reddit of comebacks ;)
+                        </h4>
+                    </div>
+                    <div id="vids">
+                        <iframe
+                            width="100%"
+                            height="315"
+                            src="https://www.youtube.com/embed/tgbNymZ7vqY"
+                        ></iframe>
+                        <iframe
+                            width="100%"
+                            height="315"
+                            src="https://www.youtube.com/embed/tgbNymZ7vqY"
+                        ></iframe>
+                    </div>
+                    <div id="outro">
+                        <h3>
+                            So what are you waiting for? Dive right into the
+                            comeback sensation that will take over the internet!
+                        </h3>
+                    </div>
+                </div>
             </aside>
-            <div
-                style={{
-                    textAlign: 'center',
-                    marginTop: '100px',
-                    color: 'whitesmoke',
-                    fontWeight: 'bold',
-                    fontSize: '20px',
-                }}
-            >
-                So what are you waiting for? Dive right into the comeback
-                sensation that will take over the internet!
-            </div>
             <footer style={{ textAlign: 'center' }}>
                 <p>The Comeback Trio &#169; 2021</p>
             </footer>
